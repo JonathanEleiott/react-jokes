7eb637557464af9d0b46d2d62052a1cd4f088a9b
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2023-07-31 10:01:40 -0400

    add joke list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,55 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [jokeList, setJokeList] = useState([]);
+
+  useEffect(() => {
+    const getJoke = async() => {
+      const response = await fetch(`https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`);
+      const data = await response.json();
+
+      const joke = {
+        id: data.id,
+        category: data.category,
+        joke: data.joke
+      }
+
+      setJokeList([...jokeList, joke])
+    }
+    getJoke();
+  }, [])
+
+  const addJoke = async() => {
+    const response = await fetch(`https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`);
+      const data = await response.json();
+
+      const joke = {
+        id: data.id,
+        category: data.category,
+        joke: data.joke
+      }
+
+      setJokeList([...jokeList, joke])
+  }
+
   return (
-    <h1>Hello Worldy</h1>
+    <>
+      <h1>React Jokes</h1>
+      
+      <ul>
+      {
+        jokeList.map((singleJoke) => {
+          console.log(singleJoke);
+          return <li key={singleJoke.id}>
+            <h2>Category: {singleJoke.category}</h2>
+            <p>{singleJoke.joke}</p>
+          </li>
+        })
+      }
+      </ul>
+
+      <button onClick={addJoke}>Add Joke</button>
+    </>
   )
 }
 
